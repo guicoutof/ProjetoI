4e62b1caf142ef78eac48247651de2c21d80b927
GuiCouto <gui_coutof@hotmail.com>
2017-12-20 23:15:56 -0200

    Finalização

--- projeto_base/application/views/footer.php
+++
@@ -1,6 +0,0 @@
-<div id="footer" class="homebody espaco-vertical alinhado-centro" style="position: absolute; bottom: 0px; width: 100%;">
-<!-- Um footer simples -->
-	&copy;Projeto I <?php echo date("Y");?> 
-
-</div>
-</div>
